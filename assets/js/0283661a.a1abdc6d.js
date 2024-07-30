e51d122d89e11c2bfc0bc2fc115cf4bf74ebac23
HowManySmall <awesomedude5475@gmail.com>
2024-07-30 17:57:37 -0600

    Built and published by Moonwave

---
+++ assets/js/0283661a.a1abdc6d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[280],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Signal","label":"\u200bSignal"}]')}}]);
---
+++ assets/js/379de215.9a6dfb5f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[320],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/howmanysmall/BetterSignal/blob/main","baseUrl":"/BetterSignal/","classOrder":["Signal"],"apiCategories":[]}')}}]);
